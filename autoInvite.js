05324fa16eaa144206bb527adaa95c4f675cc6b4
Michael Andreasen <michael@95m.dk>
2015-06-21 19:08:19 +0200

    The script

---
+++ autoInvite.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         Steam Automatic Group Invite
+// @namespace    http://www.95m.dk/
+// @version      1.0.0
+// @description  This script do it easy for you, when you like to invite some people to your Steam group.
+// @author       Michael 
+// @match        *://steamcommunity.com/id/*
+// @match        *://steamcommunity.com/profile/*
+// @require      http://code.jquery.com/jquery-2.1.4.min.js
+// @downloadURL  https://github.com/mha1995/SteamAutomaticGroupInvite
+// @updateURL    https://raw.githubusercontent.com/mha1995/SteamAutomaticGroupInvite/master/autoInvite.js
+// ==/UserScript==
+
+// Set the group custom URL you want people to be invited to. (NOT THE FULL URL ONLY CUSTOM URL)
+var steam_group_custom_url = "customURL";
+
+function InviteUserToSteamGroup(group_id)
+{
+	var params = {
+		json: 1,
+		type: 'groupInvite',
+		group: group_id,
+		sessionID: g_sessionID,
+		invitee: g_rgProfileData.steamid
+	};
+
+	$.ajax({
+		url: 'http://steamcommunity.com/actions/GroupInvite',
+		data: params,
+		type: 'POST',
+		dataType: 'json'
+	}).done(function(data) {
+		if (data.duplicate) {
+			console.log('[' + g_rgProfileData.steamid + '] The user are already in the group or have already received invites.');
+		} else {
+			console.log('[' + g_rgProfileData.steamid + '] Invite to Join Your Group.');
+		}
+	}).fail(function() {
+		console.log('Error processing your request. Please try again.');
+	});
+}
+
+function GetGroupData(steam_group_custom_url)
+{
+	return $.ajax({
+		url: 'http://steamcommunity.com/groups/' + steam_group_custom_url + '/memberslistxml',
+		data: { xml:1 },
+		type: 'GET',
+		dataType: 'xml'
+	}).done(function(xml) {
+		InviteUserToSteamGroup($(xml).find('groupID64').text());
+	}).fail(function() {
+		console.log('The request failed or the group custom URL is wrong.');
+	});
+}
+
+// Start invite process
+GetGroupData(steam_group_custom_url);
